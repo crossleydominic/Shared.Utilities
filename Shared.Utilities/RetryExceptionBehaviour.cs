277590a16621f6bb80cd312b431c86893e0ee6c4
Dominic Crossley <crossleydominic@hotmail.com>
2011-11-04 08:54:09 +0000

    Modified Retry.cs to accept an enum value and to prevent exception collation if specified

---
+++ Shared.Utilities/RetryExceptionBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Utilities
+{
+    public enum RetryExceptionBehaviour
+    {
+        /// <summary>
+        /// Exceptions are not handled at all and are immeditately
+        /// thrown to calling code
+        /// </summary>
+        DoNotHandle = 1,
+
+        /// <summary>
+        /// Exceptions are caught by Retry and added to a list of thrown exceptions
+        /// that is available in the OperationResult
+        /// </summary>
+        HandleAndCollate = 2,
+
+        /// <summary>
+        /// Exceptions are caught but they discarded. A summary of the number of exceptions
+        /// thrown is added to the OperationResult.
+        /// </summary>
+        HandleOnly = 3
+    }
+}
