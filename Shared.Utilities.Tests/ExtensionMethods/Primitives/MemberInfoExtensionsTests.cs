6ca8ba5696f4af78943afddc7a47e5071c28ad26
Dominic Crossley <crossleydominic@hotmail.com>
2012-03-27 08:27:06 +0100

    Added extension method for getting custom attributes

---
+++ Shared.Utilities.Tests/ExtensionMethods/Primitives/MemberInfoExtensionsTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shared.Utilities.ExtensionMethods.Primitives;
+using NUnit.Framework;
+
+namespace Shared.Utilities.Tests.ExtensionMethods.Primitives
+{
+    [TestFixture]
+    public class MemberInfoExtensionsTests
+    {
+        #region Test Classes
+
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple=true)]
+        public class Stub1Attribute : Attribute { }
+
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+        public class Stub2Attribute : Attribute { }
+
+        [Stub1]
+        public class StubClass1
+        {
+        }
+
+        public class InheritStubClass1 : StubClass1
+        {
+        }
+
+        [Stub1]
+        [Stub1]
+        [Stub2]
+        [Stub2]
+        public class StubClass2
+        {
+        }
+
+        #endregion
+
+        #region GetCustomAttribute Tests
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetCustomAttribute_MemberInfoNull()
+        {
+            Type t = null;
+            t.GetCustomAttribute<Stub1Attribute>();
+        }
+
+        [Test]
+        public void GetCustomAttribute_AttributeSpecified()
+        {
+            Stub1Attribute attr = typeof(StubClass1).GetCustomAttribute<Stub1Attribute>();
+
+            Assert.NotNull(attr);
+        }
+
+        [Test]
+        public void GetCustomAttribute_AttributeSpecified_WithInherit()
+        {
+            Stub1Attribute attr = typeof(InheritStubClass1).GetCustomAttribute<Stub1Attribute>();
+
+            Assert.NotNull(attr);
+        }
+
+        [Test]
+        public void GetCustomAttribute_AttributeSpecified_WithoutInherit()
+        {
+            Stub1Attribute attr = typeof(InheritStubClass1).GetCustomAttribute<Stub1Attribute>(false);
+
+            Assert.IsNull(attr);
+        }
+
+        #endregion
+
+        #region GetCustomAttributes Tests
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetCustomAttributes_MemberInfoNull()
+        {
+            Type t = null;
+            t.GetCustomAttributes<Stub1Attribute>();
+        }
+
+        [Test]
+        public void GetCustomAttributes_AttributeSpecified()
+        {
+            IList<Stub1Attribute> attrs = typeof(StubClass1).GetCustomAttributes<Stub1Attribute>();
+
+            Assert.NotNull(attrs);
+            Assert.AreEqual(1, attrs.Count);
+            Assert.NotNull(attrs[0]);
+        }
+
+        [Test]
+        public void GetCustomAttributes_AttributeSpecified_WithInherit()
+        {
+            IList<Stub1Attribute> attrs = typeof(InheritStubClass1).GetCustomAttributes<Stub1Attribute>();
+
+            Assert.NotNull(attrs);
+            Assert.AreEqual(1, attrs.Count);
+            Assert.NotNull(attrs[0]);
+        }
+
+        [Test]
+        public void GetCustomAttributes_AttributeSpecified_WithoutInherit()
+        {
+            IList<Stub1Attribute> attrs = typeof(InheritStubClass1).GetCustomAttributes<Stub1Attribute>(false);
+
+            Assert.NotNull(attrs);
+            Assert.AreEqual(0, attrs.Count);
+        }
+
+        [Test]
+        public void GetCustomAttributes_AttributeSpecifiedMoreThanOnce()
+        {
+            IList<Stub1Attribute> attrs = typeof(StubClass2).GetCustomAttributes<Stub1Attribute>();
+
+            Assert.NotNull(attrs);
+            Assert.AreEqual(2, attrs.Count);
+            Assert.NotNull(attrs[0]);
+            Assert.NotNull(attrs[1]);
+        }
+
+        #endregion
+    }
+}
---
+++ Shared.Utilities/ExtensionMethods/Primitives/MemberInfoExtensions.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Shared.Utilities.ExtensionMethods.Primitives
+{
+    /// <summary>
+    /// A set of extensions for the Type class
+    /// </summary>
+    public static class MemberInfoExtensions
+    {
+        /// <summary>
+        /// Returns the first custom attribute of the specified type, T.
+        /// Parent types are also searched.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The custom attribute type to find
+        /// </typeparam>
+        /// <param name="member">
+        /// The MemberInfo object to search
+        /// </param>
+        /// <returns>
+        /// The first matching custom attribute or null if a match couldn't be found.
+        /// </returns>
+        public static T GetCustomAttribute<T>(this MemberInfo member)
+        {
+            return GetCustomAttribute<T>(member, true);
+        }
+
+        /// <summary>
+        /// Returns the first custom attribute of the specified type, T
+        /// </summary>
+        /// <typeparam name="T">
+        /// The custom attribute type to find
+        /// </typeparam>
+        /// <param name="member">
+        /// The MemberInfo object to search
+        /// </param>
+        /// <param name="inherit">
+        /// Whether or not to include parent types in the search.
+        /// </param>
+        /// <returns>
+        /// The first matching custom attribute or null if a match couldn't be found.
+        /// </returns>
+        public static T GetCustomAttribute<T>(this MemberInfo member, bool inherit)
+        {
+            return GetCustomAttributes<T>(member, inherit).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the all custom attributes of the specified type, T.
+        /// Parent types are also searched.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The custom attribute type to find
+        /// </typeparam>
+        /// <param name="member">
+        /// The MemberInfo object to search
+        /// </param>
+        /// <param name="inherit">
+        /// Whether or not to include parent types in the search.
+        /// </param>
+        /// <returns>
+        /// A list containing all matching custom attributes.
+        /// </returns>
+        public static IList<T> GetCustomAttributes<T>(this MemberInfo member)
+        {
+            return GetCustomAttributes<T>(member, true);
+        }
+
+        /// <summary>
+        /// Returns the all custom attributes of the specified type, T
+        /// </summary>
+        /// <typeparam name="T">
+        /// The custom attribute type to find
+        /// </typeparam>
+        /// <param name="member">
+        /// The MemberInfo object to search
+        /// </param>
+        /// <param name="inherit">
+        /// Whether or not to include parent types in the search.
+        /// </param>
+        /// <returns>
+        /// A list containing all matching custom attributes.
+        /// </returns>
+        public static IList<T> GetCustomAttributes<T>(this MemberInfo member, bool inherit)
+        {
+            #region Input validation
+
+            Insist.IsNotNull(member, "member");
+
+            #endregion
+
+            object[] attributes = member.GetCustomAttributes(inherit);
+
+            List<T> matching = new List<T>();
+
+            foreach (object attr in attributes)
+            {
+                if (attr is T)
+                {
+                    matching.Add((T)attr);
+                }
+            }
+
+            return matching;
+        }
+    }
+}
