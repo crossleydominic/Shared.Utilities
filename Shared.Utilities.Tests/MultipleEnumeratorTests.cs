de740b5767c5312f94f4f24d4dcfc84b2f936df7
Dom Crossley <dom.crossley@pebblecode.com>
2013-03-27 16:39:14 +0000

    Added MultipleEnumerator

---
+++ Shared.Utilities.Tests/MultipleEnumeratorTests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Utilities.Tests
+{
+    [TestFixture]
+    public class MultipleEnumeratorTests
+    {
+        #region Constructor tests
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_NullEnumerables()
+        {
+            MultipleEnumerator<int> subject = new MultipleEnumerator<int>(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_EmptyEnumerables()
+        {
+            MultipleEnumerator<int> subject = new MultipleEnumerator<int>();
+        }
+
+        #endregion
+
+        [Test]
+        public void GetEnumerator_OneList()
+        {
+            List<int> list1 = new List<int>(){1,2,3};
+
+            MultipleEnumerator<int> subject = new MultipleEnumerator<int>(list1);
+
+            int counter = 1;
+            foreach (int i in subject)
+            {
+                Assert.AreEqual(counter, i);
+                counter += 1;
+            }
+        }
+
+        [Test]
+        public void GetEnumerator_MultipleLists()
+        {
+            List<int> list1 = new List<int>() { 1, 2, 3 };
+            List<int> list2 = new List<int>() { 4, 5, 6 };
+            List<int> list3 = new List<int>() { 7, 8, 9 };
+
+            MultipleEnumerator<int> subject = new MultipleEnumerator<int>(list1, list2, list3);
+
+            int counter = 1;
+            foreach (int i in subject)
+            {
+                Assert.AreEqual(counter, i);
+                counter += 1;
+            }
+        }
+
+        [Test]
+        public void GetEnumerator_MultipleListsWithDifferentImplementations()
+        {
+            List<DerivedA> list1 = new List<DerivedA>() { new DerivedA(1), new DerivedA(2), new DerivedA(3) };
+            List<DerivedB> list2 = new List<DerivedB>() { new DerivedB(4), new DerivedB(5), new DerivedB(6) };
+
+            MultipleEnumerator<Base> subject = new MultipleEnumerator<Base>(list1, list2);
+
+            int counter = 1;
+            foreach (Base b in subject)
+            {
+                Assert.AreEqual(counter, b.Value());
+                counter += 1;
+            } 
+        }
+
+        public abstract class Base
+        {
+            public abstract int Value();
+        }
+
+        public class DerivedA : Base
+        {
+            private int _value;
+            public DerivedA(int value)
+            {
+                _value = value;
+            }
+            public override int Value()
+            {
+                return _value;
+            }
+        }
+
+        public class DerivedB : Base
+        {
+            private int _value;
+
+            public DerivedB(int value)
+            {
+                _value = value;
+            }
+
+            public override int Value()
+            {
+                return _value;
+            }
+        }
+
+    }
+}
---
+++ Shared.Utilities/MultipleEnumerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Utilities
+{
+    /// <summary>
+    /// Class that allows a single foreach to enumerate through a set of lists, enumerating each in turn
+    /// </summary>
+    public class MultipleEnumerator<T> : IEnumerable<T>
+    {
+        #region Private members
+
+        /// <summary>
+        /// The set of enumerables to enumerate through
+        /// </summary>
+        private IEnumerable<T>[] _enumerables;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Create new instance that will enumerate through the supplied set of enumerables
+        /// </summary>
+        public MultipleEnumerator(params IEnumerable<T>[] enumerables)
+        {
+            #region Input validation
+
+            Insist.IsNotNull(enumerables, "enumerables");
+            Insist.IsNotEmpty(enumerables, "enumerables");
+
+            #endregion
+
+            _enumerables = enumerables;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Get an enumerator to loop through each of the items in all of the enumerables in turn
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (IEnumerable<T> enumerable in _enumerables)
+            {
+                foreach (T value in enumerable)
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
