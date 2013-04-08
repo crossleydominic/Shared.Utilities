934ea9263ff9e2267cd423c9b1e95346310887ab
Dom Crossley <dom.crossley@pebblecode.com>
2013-04-08 15:39:00 +0100

    Added Either type

---
+++ Shared.Utilities.Tests/EitherTests.cs
@@ -0,0 +1,337 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Utilities.Tests
+{
+    [TestFixture]
+    public class EitherTests
+    {
+        #region CreateLeft Tests
+
+        [Test]
+        public void CreateLeft_EitherHasLeftValue()
+        {
+            var result = Either<string, bool>.CreateLeft("value");
+
+            Assert.IsFalse(result.HasRight);
+            Assert.AreEqual("value", result.LeftValue);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CreateLeft_EitherDoesNotHaveRightValue()
+        {
+            var result = Either<string, bool>.CreateLeft("value");
+
+            Assert.IsFalse(result.HasRight);
+
+            var temp = result.RightValue;
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), MatchType = MessageMatch.Contains, ExpectedMessage = "leftValue")]
+        public void CreateLeft_NullValueThrows()
+        {
+            var result = Either<string, bool>.CreateLeft(null);
+        }
+
+        #endregion
+
+        #region CreateRight Tests
+
+        [Test]
+        public void CreateRight_EitherHasRightValue()
+        {
+            var result = Either<string, bool>.CreateRight(true);
+
+            Assert.IsTrue(result.HasRight);
+            Assert.AreEqual(true, result.RightValue);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CreateRight_EitherDoesNotHaveLeftValue()
+        {
+            var result = Either<string, bool>.CreateRight(true);
+
+            Assert.IsTrue(result.HasRight);
+
+            var temp = result.LeftValue;
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), MatchType = MessageMatch.Contains, ExpectedMessage = "rightValue")]
+        public void CreateRight_NullValueThrows()
+        {
+            var result = Either<string, object>.CreateRight(null);
+        }
+
+        #endregion
+
+        #region Apply Tests
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage = "leftAction", MatchType = MessageMatch.Contains)]
+        public void Apply_NoReturn_NullLeftActionThrows()
+        {
+            var result = Either<string, int>.CreateLeft("value");
+
+            result.Apply(null, (r) => { });
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage = "rightAction", MatchType = MessageMatch.Contains)]
+        public void Apply_NoReturn_NullRightActionThrows()
+        {
+            var result = Either<string, int>.CreateRight(1);
+
+            result.Apply(l => { }, null);
+        }
+
+        [Test]
+        public void Apply_NoReturn_LeftActionRun()
+        {
+            var result = Either<int, int>.CreateLeft(1);
+
+            bool leftBranchTaken = false;
+            bool rightBranchTaken = false;
+            result.Apply(l => { leftBranchTaken = true; }, r => { rightBranchTaken = false; });
+
+            Assert.IsTrue(leftBranchTaken);
+            Assert.IsFalse(rightBranchTaken);
+        }
+
+        [Test]
+        public void Apply_NoReturn_RightActionRun()
+        {
+            var result = Either<int, int>.CreateRight(1);
+
+            bool leftBranchTaken = false;
+            bool rightBranchTaken = false;
+            result.Apply(l => { leftBranchTaken = false; }, r => { rightBranchTaken = true; });
+
+            Assert.IsFalse(leftBranchTaken);
+            Assert.IsTrue(rightBranchTaken);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage = "leftFunc", MatchType = MessageMatch.Contains)]
+        public void Apply_WithReturn_NullLeftActionThrows()
+        {
+            var result = Either<string, int>.CreateLeft("value");
+
+            result.Apply(null, (r) => 1);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage = "rightFunc", MatchType = MessageMatch.Contains)]
+        public void Apply_WithReturn_NullRightActionThrows()
+        {
+            var result = Either<string, int>.CreateRight(1);
+
+            result.Apply(l => 1, null);
+        }
+
+        [Test]
+        public void Apply_WithReturn_LeftFuncRun()
+        {
+            var result = Either<int, int>.CreateLeft(1);
+
+            int branchOutput = -1;
+            branchOutput = result.Apply(l => 1, r => 2);
+
+            Assert.AreEqual(1, branchOutput);
+        }
+
+        [Test]
+        public void Apply_WithReturn_RightFuncRun()
+        {
+            var result = Either<int, int>.CreateRight(1);
+
+            int branchOutput = -1;
+            branchOutput = result.Apply(l => 1, r => 2);
+
+            Assert.AreEqual(2, branchOutput);
+        }
+
+        #endregion
+
+        #region HasRight Tests
+
+        [Test]
+        public void HasRight_TrueWhenCreatedWithRight()
+        {
+            var result = Either<int, int>.CreateRight(1);
+
+            Assert.IsTrue(result.HasRight);
+        }
+
+        [Test]
+        public void HasRight_FalseWhenCreatedWithLeft()
+        {
+            var result = Either<int, int>.CreateLeft(1);
+
+            Assert.IsFalse(result.HasRight);
+        }
+
+        #endregion
+
+        #region LeftValue Tests
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void LeftValue_ThrowsWhenCreatedAsRight()
+        {
+            var result = Either<int, int>.CreateRight(1);
+
+            var temp = result.LeftValue;
+        }
+
+        [Test]
+        public void LeftValue_DoesNotThrowWhenCreatedAsLeft()
+        {
+            var result = Either<int, int>.CreateLeft(1);
+
+            Assert.AreEqual(1, result.LeftValue);
+        }
+
+        #endregion
+
+        #region RightValue Tests
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void RightValue_ThrowsWhenCreatedAsLeft()
+        {
+            var result = Either<int, int>.CreateLeft(1);
+
+            var temp = result.RightValue;
+        }
+
+        [Test]
+        public void RightValue_DoesNotThrowWhenCreatedAsRight()
+        {
+            var result = Either<int, int>.CreateRight(1);
+
+            Assert.AreEqual(1, result.RightValue);
+        }
+
+        #endregion
+
+        #region Lefts Tests
+
+        [Test]
+        [ExpectedException(typeof (ArgumentNullException), MatchType = MessageMatch.Contains, ExpectedMessage = "eithers")]
+        public void Lefts_NullCollectionThrows()
+        {
+            Either.Lefts<string,int>(null);
+        }
+
+        [Test]
+        public void Lefts_EmptyCollectionDoesNotThrow()
+        {
+            var result = Either.Lefts<string, int>(new List<Either<string, int>>());
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Any());
+        }
+
+        [Test]
+        public void Lefts_LeftValuesCollated()
+        {
+            var result = Either.Lefts<string, int>(new List<Either<string, int>>()
+            {
+                Either<string,int>.CreateLeft("1"),
+                Either<string,int>.CreateRight(2),
+                Either<string,int>.CreateLeft("3"),
+            });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.IsTrue(result.Any(i=>i == "1"));
+            Assert.IsTrue(result.Any(i => i == "3"));
+        }
+
+        #endregion
+
+        #region Rights Tests
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), MatchType = MessageMatch.Contains, ExpectedMessage = "eithers")]
+        public void Rights_NullCollectionThrows()
+        {
+            Either.Rights<string, int>(null);
+        }
+
+        [Test]
+        public void Rights_EmptyCollectionDoesNotThrow()
+        {
+            var result = Either.Rights<string, int>(new List<Either<string, int>>());
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Any());
+        }
+
+        [Test]
+        public void Rights_RightValuesCollated()
+        {
+            var result = Either.Rights<string, int>(new List<Either<string, int>>()
+            {
+                Either<string,int>.CreateRight(1),
+                Either<string,int>.CreateLeft("2"),
+                Either<string,int>.CreateRight(3),
+            });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.IsTrue(result.Any(i => i == 1));
+            Assert.IsTrue(result.Any(i => i == 3));
+        }
+
+        #endregion
+
+        #region Rights Tests
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), MatchType = MessageMatch.Contains, ExpectedMessage = "eithers")]
+        public void Partition_NullCollectionThrows()
+        {
+            Either.Partition<string, int>(null);
+        }
+
+        [Test]
+        public void Partition_EmptyCollectionDoesNotThrow()
+        {
+            var result = Either.Partition<string, int>(new List<Either<string, int>>());
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Item1.Any());
+            Assert.IsFalse(result.Item2.Any());
+        }
+
+        [Test]
+        public void Partition_ValuesCollated()
+        {
+            var result = Either.Partition<string, int>(new List<Either<string, int>>()
+            {
+                Either<string,int>.CreateRight(1),
+                Either<string,int>.CreateLeft("2"),
+                Either<string,int>.CreateRight(3),
+            });
+
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(1, result.Item1.Count());
+            Assert.IsTrue(result.Item1.Any(i => i == "2"));
+
+            Assert.AreEqual(2, result.Item2.Count());
+            Assert.IsTrue(result.Item2.Any(i => i == 1));
+            Assert.IsTrue(result.Item2.Any(i => i == 3));
+        }
+
+        #endregion
+    }
+}
---
+++ Shared.Utilities/Either.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Utilities
+{
+    /// <summary>
+    /// Helper class to provide type-inferred utility functions for working with Eithers
+    /// </summary>
+    public class Either
+    {
+        /// <summary>
+        /// Partition a set of Eithers into a Tuple that collates all the Left and Right values togehter
+        /// </summary>
+        /// <returns>
+        /// A tuple that contains collates set of Left and Right eithers
+        /// </returns>
+        public static Tuple<IEnumerable<TLeft>, IEnumerable<TRight>> Partition<TLeft, TRight>(IEnumerable<Either<TLeft, TRight>> eithers)
+        {
+            #region Input validation
+
+            Insist.IsNotNull(eithers, "eithers");
+
+            #endregion
+
+            return new Tuple<IEnumerable<TLeft>, IEnumerable<TRight>>(
+                Lefts(eithers),
+                Rights(eithers));
+        }
+
+        /// <summary>
+        /// Collates all Left values from a set of eithers
+        /// </summary>
+        /// <param name="eithers">
+        /// The set of eithers from which to obtain the Left values
+        /// </param>
+        /// <returns>
+        /// All the left values from the supplied set of eithers
+        /// </returns>
+        public static IEnumerable<TLeft> Lefts<TLeft, TRight>(IEnumerable<Either<TLeft, TRight>> eithers)
+        {
+            #region Input validation
+
+            Insist.IsNotNull(eithers, "eithers");
+
+            #endregion
+
+            return eithers.Where(e => !e.HasRight).Select(e => e.LeftValue);
+        }
+
+        /// <summary>
+        /// Collates all Right values from a set of eithers
+        /// </summary>
+        /// <param name="eithers">
+        /// The set of eithers from which to obtain the left values
+        /// </param>
+        /// <returns>
+        /// All the left values from the supplied set of eithers
+        /// </returns>
+        public static IEnumerable<TRight> Rights<TLeft, TRight>(IEnumerable<Either<TLeft, TRight>> eithers)
+        {
+            #region Input validation
+
+            Insist.IsNotNull(eithers, "eithers");
+
+            #endregion
+
+            return eithers.Where(e => e.HasRight).Select(e => e.RightValue);
+        }
+    }
+
+    /// <summary>
+    /// Represents either a value of one type or another. Null values are dissallowed
+    /// </summary>
+    /// <typeparam name="TLeft">The type for the Left value</typeparam>
+    /// <typeparam name="TRight">The type for the Right value</typeparam>
+    public class Either<TLeft, TRight> : Either
+    {
+        #region Private members
+
+        /// <summary>
+        /// The Left value
+        /// </summary>
+        private TLeft _leftVal;
+
+        /// <summary>
+        /// The Right value
+        /// </summary>
+        private TRight _rightVal;
+
+        /// <summary>
+        /// Whether or not this Either was constructed with a Left or Right value
+        /// </summary>
+        private bool _hasRight;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Disallow direct construction. Use static helper methods instead.
+        /// </summary>
+        protected Either() { }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Construct an Either that wraps a Left value
+        /// </summary>
+        public static Either<TLeft, TRight> CreateLeft(TLeft leftValue)
+        {
+            #region Input validation
+
+            Insist.IsNotNull(leftValue, "leftValue");
+
+            #endregion
+
+            Either<TLeft, TRight> either = new Either<TLeft, TRight>();
+            either._leftVal = leftValue;
+            either._hasRight = false;
+
+            return either;
+        }
+
+        /// <summary>
+        /// Construct an Either that wraps a Right value
+        /// </summary>
+        public static Either<TLeft, TRight> CreateRight(TRight rightValue)
+        {
+            #region Input validation
+
+            Insist.IsNotNull(rightValue, "rightValue");
+
+            #endregion
+
+            Either<TLeft, TRight> either = new Either<TLeft, TRight>();
+            either._rightVal = rightValue;
+            either._hasRight = true;
+
+            return either;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Apply(Action<TLeft> leftAction, Action<TRight> rightAction)
+        {
+            #region Input Validation
+
+            Insist.IsNotNull(leftAction, "leftAction");
+            Insist.IsNotNull(rightAction, "rightAction");
+
+            #endregion
+
+            if (_hasRight)
+            {
+                rightAction(_rightVal);
+            }
+            else
+            {
+                leftAction(_leftVal);
+            }
+        }
+
+        public TOut Apply<TOut>(Func<TLeft, TOut> leftFunc, Func<TRight, TOut> rightFunc)
+        {
+            #region Input Validation
+
+            Insist.IsNotNull(leftFunc, "leftFunc");
+            Insist.IsNotNull(rightFunc, "rightFunc");
+
+            #endregion
+
+            if (_hasRight)
+            {
+                return rightFunc(_rightVal);
+            }
+            else
+            {
+                return leftFunc(_leftVal);
+            }
+        }
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Has this Either got a Right value, if not then it will have a Left value 
+        /// </summary>
+        public bool HasRight
+        {
+            get { return _hasRight; }
+        }
+
+        /// <summary>
+        /// Gets the Left value, if it exists. If a Left value does not exist then an
+        /// exception is thrown
+        /// </summary>
+        public TLeft LeftValue
+        {
+            get
+            {
+                if (_hasRight)
+                {
+                    throw new InvalidOperationException("Cannot obtain left value for an Either constructed via Right");
+                }
+
+                return _leftVal;
+            }
+        }
+
+        /// <summary>
+        /// Gets the Right value, if it exists. If a Right value does not exist then an
+        /// exception is thrown
+        /// </summary>
+        public TRight RightValue
+        {
+            get
+            {
+                if (!_hasRight)
+                {
+                    throw new InvalidOperationException("Cannot obtain right value for an Either constructed via Left");
+                }
+
+                return _rightVal;
+            }
+        }
+
+        #endregion
+    }
+}
