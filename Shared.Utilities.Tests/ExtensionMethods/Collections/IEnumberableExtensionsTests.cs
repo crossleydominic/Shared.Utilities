66a2184628b87b698cd68f0d5802e887f451098c
Dom Crossley <dom.crossley@pebblecode.com>
2013-05-01 15:21:15 +0100

    Added extension method to chunk enumerables

---
+++ Shared.Utilities.Tests/ExtensionMethods/Collections/IEnumberableExtensionsTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shared.Utilities.ExtensionMethods.Collections;
+
+namespace Shared.Utilities.Tests.ExtensionMethods.Collections
+{
+    [TestFixture]
+    public class IEnumberableExtensionsTests
+    {
+        #region Chunk
+
+        [Test]
+        public void Chunk_NoData()
+        {
+            List<int> subject = new List<int>();
+            var chunks = subject.Chunk(10);
+
+            Assert.IsNotNull(chunks);
+            Assert.AreEqual(0, chunks.Count);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Chunk_InvalidChunkSize()
+        {
+            List<int> subject = new List<int>(){1,2,3,4,5,6};
+            var chunks = subject.Chunk(0);
+        }
+
+        [Test]
+        public void Chunk_ChunkSizeBiggerThanCollection()
+        {
+            List<int> subject = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            var chunks = subject.Chunk(10);
+
+            Assert.IsNotNull(chunks);
+            Assert.AreEqual(1, chunks.Count);
+            Assert.AreEqual(6, chunks[0].Count);
+            Assert.AreEqual(1, chunks[0][0]);
+            Assert.AreEqual(2, chunks[0][1]);
+            Assert.AreEqual(3, chunks[0][2]);
+            Assert.AreEqual(4, chunks[0][3]);
+            Assert.AreEqual(5, chunks[0][4]);
+            Assert.AreEqual(6, chunks[0][5]);
+
+        }
+
+        [Test]
+        public void Chunk_ChunkSizeMultipleOfCollectionSize()
+        {
+            List<int> subject = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            var chunks = subject.Chunk(3);
+
+            Assert.IsNotNull(chunks);
+            Assert.AreEqual(2, chunks.Count);
+
+            Assert.AreEqual(3, chunks[0].Count);
+            Assert.AreEqual(1, chunks[0][0]);
+            Assert.AreEqual(2, chunks[0][1]);
+            Assert.AreEqual(3, chunks[0][2]);
+
+            Assert.AreEqual(3, chunks[1].Count);
+            Assert.AreEqual(4, chunks[1][0]);
+            Assert.AreEqual(5, chunks[1][1]);
+            Assert.AreEqual(6, chunks[1][2]);
+        }
+
+        [Test]
+        public void Chunk_ChunkSizeNotMultipleOfCollectionSize()
+        {
+            List<int> subject = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            var chunks = subject.Chunk(4);
+
+            Assert.IsNotNull(chunks);
+            Assert.AreEqual(2, chunks.Count);
+
+            Assert.AreEqual(4, chunks[0].Count);
+            Assert.AreEqual(1, chunks[0][0]);
+            Assert.AreEqual(2, chunks[0][1]);
+            Assert.AreEqual(3, chunks[0][2]);
+            Assert.AreEqual(4, chunks[0][3]);
+
+            Assert.AreEqual(2, chunks[1].Count);
+            Assert.AreEqual(5, chunks[1][0]);
+            Assert.AreEqual(6, chunks[1][1]);
+        }
+
+        #endregion
+    }
+}
---
+++ Shared.Utilities/ExtensionMethods/Collections/IEnumerableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Utilities.ExtensionMethods.Collections
+{
+    public static class IEnumerableExtensions
+    {
+        public static IList<IList<T>> Chunk<T>(this IEnumerable<T> data, int chunkSize)
+        {
+            #region Input validation
+
+            Insist.IsAtLeast(chunkSize, 1, "chunkSize");
+
+            if (!data.Any())
+            {
+                return new List<IList<T>>();
+            }
+
+            #endregion
+
+            IList<IList<T>> chunks = new List<IList<T>>();
+            int currentIndex = 0;
+
+            List<T> currentChunk = null;
+
+            foreach (T element in data)
+            {
+                int offset = currentIndex % chunkSize;
+
+                if (offset == 0)
+                {
+                    currentChunk = new List<T>();
+                    chunks.Add(currentChunk);
+                }
+
+                currentChunk.Add(element);
+
+                currentIndex++;
+            }
+
+            return chunks;
+        }
+    }
+}
