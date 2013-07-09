08c8e47e1fb0a9ff52a5407f677aad474d03fec6
Dom Crossley <dominic.crossley@wonga.com>
2013-07-09 15:51:51 +0100

    Added SafeDeRef

---
+++ Shared.Utilities.Tests/ExtensionMethods/Object/ObjectExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shared.Utilities.ExtensionMethods.Object;
+using NUnit.Framework;
+
+namespace Shared.Utilities.Tests.ExtensionMethods.Object
+{
+    [TestFixture]
+    public class ObjectExtensionsTests
+    {
+        #region SafeDeRef tests
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SafeDeRef_DeRefFuncIsNull_Throws()
+        {
+            string str = "abcdefg";
+            int length = str.SafeDeRef<string,int>(null);
+        }
+
+        [Test]
+        public void SafeDeRef_NonNullObjectDeRefsCorrectly_ValueType()
+        {
+            string str = "abcdefg";
+            int length = str.SafeDeRef(s=>s.Length);
+
+            Assert.AreEqual(7, length);
+        }
+
+        [Test]
+        public void SafeDeRef_NullObjectDeRefsCorrectly_ValueType()
+        {
+            string str = null;
+            int length = str.SafeDeRef(s => s.Length);
+
+            Assert.AreEqual(0, length);
+        }
+
+        [Test]
+        public void SafeDeRef_NonNullObjectDeRefsCorrectly_ReferenceType()
+        {
+            string str = "abcdefg";
+            string result = str.SafeDeRef(s => s.Substring(1));
+
+            Assert.AreEqual("bcdefg", result);
+        }
+
+        [Test]
+        public void SafeDeRef_NullObjectDeRefsCorrectly_ReferenceType()
+        {
+            string str = null;
+            string result = str.SafeDeRef(s => s.Substring(1));
+
+            Assert.AreEqual(null, result);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Shared.Utilities/ExtensionMethods/Object/ObjectExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Utilities.ExtensionMethods.Object
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Allows properties on an object to be safely dereferenced even if the object is null. Not advised for production code, 
+        /// more for use in prototyping code.  If the object is null then the null is propogated and a NullRefEx is not thrown.
+        /// </summary>
+        /// <param name="obj">The object to dereference</param>
+        /// <param name="deRefFunc">The value obtaining function</param>
+        /// <returns>
+        /// The value obtained from the deRefRunction if the object was not null. Otherwise null.
+        /// </returns>
+        public static TOut SafeDeRef<TIn, TOut>(this TIn obj, Func<TIn, TOut> deRefFunc)
+            where TIn : class
+        {
+            #region Input validation
+
+            Insist.IsNotNull(deRefFunc, "deRefFunc");
+
+            #endregion
+
+            if (obj == null)
+            {
+                return default(TOut);
+            }
+
+            return deRefFunc(obj);
+        }
+    }
+}
